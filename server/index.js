44052af304662a483ff5e757b8921d443ef4da7d
rebecca-millerr <me@beccamiller.com>
2021-03-28 17:59:08 -0400

    boilerplate + deployment

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const apiPort = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+// this is for creating static webpage
+app.use(express.static(path.join(__dirname, "/../client/build")));
+
+// this is for creating static webpage in prod
+if (process.env.PORT) {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+    });
+}
+else { // dev
+    app.get('/', (req, res) => {
+        res.send("Hello world!");
+    });
+}
+
+app.listen(apiPort, () => console.log("server running on port 5000"));
