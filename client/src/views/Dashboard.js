542e3e07ea5bf9b4247cc8c35face75bd6e4e4e5
rebecca-millerr <me@beccamiller.com>
2021-04-11 00:43:30 -0400

    ugly FE

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { getSheetData } from '../api';
+import './Dashboard.css';
 
 const Dashboard = () => {
+    const [data, setData] = useState([[]]);
+    
     useEffect(() => {
         const getData = async () => {
             const res = await getSheetData();
-            console.log(res);
+            setData(res.data.data);
         }
         getData();
     }, [])
 
     return (
-        <div>Hello world</div>
+        <div>
+            <h1>This is some test data</h1>
+            <div className="test-table">
+                {data.map((row, rindex) => 
+                    <React.Fragment key={rindex}>
+                        {Object.values(row).map((col, cindex) =>
+                            <div key={cindex}>{col}</div>
+                        )}
+                    </React.Fragment>
+                )}
+            </div>
+        </div>
     )
 }
 
